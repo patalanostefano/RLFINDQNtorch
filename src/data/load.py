ae29d431353b4b723b95a99abc725c26c0ce5d4d
patalanostefano <136838737+patalanostefano@users.noreply.github.com>
2024-11-24 15:11:34 +0100

    Update load.py

--- src/data/load.py
+++ src/data/load.py
@@ -56,7 +56,7 @@ class DataLoader:
             'High': 'high',
             'Low': 'low',
             'Open': 'open',
-            'Volume': 'Volume'
+            'Volume': 'volume'
         }, inplace=True)
         
         zero_close = data[data['close'] == 0]
